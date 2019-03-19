499291acbf38dd3732269bf9225bb98713f93b12
Diep Quynh <remilia.1505@gmail.com>
2019-03-19 12:40:20 +0700

    gaming_control: Remove task kill check
    In case task_struct was locked out, we can't check its cmdline
    If we do, we'll get a soft lockup
    
    Background and top-app check is enough
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -38,9 +38,6 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/signal.h>
 
-/* Gaming control */
-#include <linux/gaming_control.h>
-
 #include <asm/param.h>
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
@@ -1200,9 +1197,6 @@ int do_send_sig_info(int sig, struct siginfo *info, struct task_struct *p,
 	if ((sig == SIGKILL || sig == SIGTERM || sig == SIGABRT || sig == SIGQUIT)) 
 		sig_report(current, p);
 #endif
-
-	if ((sig == SIGKILL || sig == SIGTERM || sig == SIGABRT || sig == SIGQUIT)) 
-		game_option(p, GAME_KILLED);
 
 	if (lock_task_sighand(p, &flags)) {
 		ret = send_signal(sig, info, p, group);
