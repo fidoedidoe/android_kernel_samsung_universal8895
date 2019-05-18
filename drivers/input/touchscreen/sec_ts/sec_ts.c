3bbea1d04114964be093d521e77200e9f1c3a9c6
FAROVITUS <farovitus@gmail.com>
2019-05-18 18:02:00 +0300

    import G95XFXXU4DSDA OSRC
    Signed-off-by: FAROVITUS <farovitus@gmail.com>

--- drivers/input/touchscreen/sec_ts/sec_ts.c
+++ drivers/input/touchscreen/sec_ts/sec_ts.c
@@ -2043,6 +2043,7 @@ static void sec_ts_set_input_prop(struct sec_ts_data *ts, struct input_dev *dev,
 	set_bit(BTN_TOUCH, dev->keybit);
 	set_bit(BTN_TOOL_FINGER, dev->keybit);
 	set_bit(KEY_BLACK_UI_GESTURE, dev->keybit);
+	set_bit(KEY_INT_CANCEL, dev->keybit);
 #ifdef SEC_TS_SUPPORT_TOUCH_KEY
 	if (ts->plat_data->support_mskey) {
 		int i;
@@ -2812,18 +2813,24 @@ static void sec_ts_input_close(struct input_dev *dev)
 
 	ts->pressure_setting_mode = 0;
 
-	if (ts->lowpower_mode) {
-		int ret;
-
-		ret = sec_ts_set_lowpowermode(ts, TO_LOWPOWER_MODE);
-		if (ts->reset_is_on_going && (ret < 0)) {
-			input_err(true, &ts->client->dev, "%s: failed to reset, ret:%d\n", __func__, ret);
-			ts->reset_is_on_going = false;
-			queue_delayed_work(system_power_efficient_wq, &ts->reset_work, msecs_to_jiffies(TOUCH_RESET_DWORK_TIME));
-		}
-	} else {
+	if (ts->prox_power_off) {
 		sec_ts_stop_device(ts);
+	} else {
+		if (ts->lowpower_mode) {
+			int ret;
+
+			ret = sec_ts_set_lowpowermode(ts, TO_LOWPOWER_MODE);
+			if (ts->reset_is_on_going && (ret < 0)) {
+				input_err(true, &ts->client->dev, "%s: failed to reset, ret:%d\n", __func__, ret);
+				ts->reset_is_on_going = false;
+				queue_delayed_work(system_power_efficient_wq, &ts->reset_work, msecs_to_jiffies(TOUCH_RESET_DWORK_TIME));
+			}
+		} else {
+			sec_ts_stop_device(ts);
+		}
 	}
+
+	ts->prox_power_off = 0;
 }
 #endif
 
@@ -2907,9 +2914,17 @@ int sec_ts_stop_device(struct sec_ts_data *ts)
 		goto out;
 	}
 
+	disable_irq(ts->client->irq);
+
 	ts->power_status = SEC_TS_STATE_POWER_OFF;
 
-	disable_irq(ts->client->irq);
+	if (ts->prox_power_off) {
+		input_report_key(ts->input_dev, KEY_INT_CANCEL, 1);
+		input_sync(ts->input_dev);
+		input_report_key(ts->input_dev, KEY_INT_CANCEL, 0);
+		input_sync(ts->input_dev);
+	}
+
 	sec_ts_locked_release_all_finger(ts);
 
 	ts->plat_data->power(ts, false);
