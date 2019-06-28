67dcfeb79174e91988eb04a636e99a51bfb65aec
Diep Quynh <remilia.1505@gmail.com>
2019-06-28 19:08:56 +0700

    cpu_input_boost: Increase top-app transition boost period
    *extra smooth apps switching performance*
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2782,7 +2782,7 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 	if (!ret && !threadgroup &&
 	    !strcmp(of->kn->parent->name, "top-app") &&
 	    is_zygote_pid(tsk->parent->pid)) {
-		cpu_input_boost_kick_max(500);
+		cpu_input_boost_kick_max(1250);
 	}
 
 	put_task_struct(tsk);
